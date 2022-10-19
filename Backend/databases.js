4dd19c99369e7e33f56e13801c372af5e22ce0a2
Yasmin <ych.ascom@gmail.com>
2022-10-19 18:00:19 -0300

    frontend start

--- Backend/databases.js
+++ Backend/databases.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'tacos_shop',
+    database: 'tapas_shop',
     password: 'admin',
     port: 5432
 });
