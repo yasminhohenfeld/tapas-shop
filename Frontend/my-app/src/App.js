92a65209bb93d0891668a4e976c12144fef54a5c
yasminhohenfeld <ych.ascom@gmail.com>
2022-12-03 11:18:39 -0300

    start frontend

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bem-vindo a loja de Tapas!
+      </header>
+    </div>
+  );
+}
+
+export default App;
