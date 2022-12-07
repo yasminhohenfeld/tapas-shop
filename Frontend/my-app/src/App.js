c96d1db1b8c9dc01d8a2d0e62e36be3093a73716
yasminhohenfeld <ych.ascom@gmail.com>
2022-12-07 11:17:57 -0300

    frontend formulario

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -2,12 +2,47 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div class="App-header">
       <div>
+
+        <div class= "App-text">
         Bem-vindo a loja de Tapas!
+        </div>
+
+        <div class="App-image">
         <img src='tapas-espanholas.webp' alt= "" />
+        </div>
 
+        <p class="App-text-two">
 
+          <form>
+            <div>
+            <label>
+              Nome:
+              <input type="text" name="name" required />
+            </label>
+            </div>
+            
+            <div>
+            <label>
+              Email:
+              <input type="email" name="email" required />
+            </label>
+            </div>
+
+            <div>
+            <label>
+              Senha:
+              <input type="senha" name="senha" required />
+            </label>
+            </div>
+                  
+            <input type="submit" value="Cadastrar" />
+          </form>
+         
+        </p>
+
+        
       </div>
 
     </div>
